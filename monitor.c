a3001124b8c9225ef79dbf60d45f39d30a4a71a6
N. Neumann <vatriani.nn@googlemail.com>
2024-09-24 20:49:26 +0200

    Add signal handling for strg+c Add universal command to char* functions Add getting values

--- monitor.c
+++ monitor.c
@@ -2,7 +2,7 @@
   *  \file       monitor.c
   *  \brief      Implements the watchdog for accuiring and sending data.
   *  \author     Niels Neumann
-  *  \version    0.1
+  *  \version    0.2
   *  \date       2024
   *  \copyright  GNU Public License v3
   *  \pre        First initialize the system.
@@ -21,8 +21,10 @@
 #include <termios.h>
 #include <unistd.h>
 #include <bits/getopt_ext.h>
+#include <signal.h>
 
 #include "include/s_buffer.h"
+
 
 
 /// serial port handler
@@ -42,14 +44,179 @@ char* devicePath = "/dev/ttyUSB0";
 
 
 
-void getValues ( ) {
+void getLineFromCommand ( char** rstr, char* command ) {
+	FILE *fp;
+  char retstr[1035];
 
+	// Open the command for reading
+  fp = popen ( command, "r" );
+  if ( fp == NULL ) {
+	  printf ( "Failed to run command\n" );
+	  exit ( errno );
+  }
+
+  // Read the output
+  fgets ( retstr, sizeof ( retstr ), fp );
+
+	pclose(fp);
+
+	(*rstr) = malloc ( strlen ( retstr ) );
+	memcpy ( (*rstr), retstr, strlen (retstr) );
 }
 
 
 
-void parseToBuffer ( char ** out ) {
+void getLinesFromCommand ( char** rstr, char* command ) {
+  FILE *fp;
+  char retstr[1035];
+	char strbuff[6000];
 
+  // Open the command for reading
+  fp = popen ( command, "r" );
+  if ( fp == NULL ) {
+    printf ("Failed to run command\n" );
+    exit ( 1 );
+  }
+
+	strcpy ( strbuff, "" );
+
+  // Read the output a line at a time - append it to strbuff
+  while ( fgets ( retstr, sizeof ( retstr ), fp ) != NULL ) {
+	  strcat( strbuff, retstr );
+  }
+
+  // close
+  pclose(fp);
+
+	(*rstr) = malloc ( strlen ( strbuff ) );
+	memcpy ( (*rstr), strbuff, strlen (strbuff) );
+}
+
+
+
+void getCpuValues ( ) {
+	char* commandRet;
+
+	getLineFromCommand (
+		&commandRet,
+		"cat /proc/cpuinfo | grep \"cpu MHz\" | head -n1 | cut -d\" \" -f3 | cut -d\".\" -f1");
+
+	memcpy ( (void*) buff.cpuLines[1], commandRet, strlen ( commandRet ) - 1 );
+
+	getLineFromCommand (
+		&commandRet,
+		"sensors | grep Tctl | cut -d\"+\" -f2" );
+
+	memcpy ( (void*) buff.cpuLines[0], commandRet, strlen ( commandRet ) -6  );
+
+	getLineFromCommand (
+		&commandRet,
+		"top -n1 | head -n3 | tail -n1 | cut -d\" \" -f11" );
+
+	float tmp = atof ( commandRet );
+		gcvt( 100.0 - tmp ,4 , (char*) buff.cpuLines[2]);
+}
+
+void getGpuValues ( ) {
+	char* commandRet;
+	char* myPtr;
+	int counter;
+
+  getLineFromCommand (
+		&commandRet,
+		"/usr/bin/nvidia-smi | head -n10 | tail -n1");
+
+	// divide string
+	myPtr = strtok( commandRet, " ");
+	counter = 0;
+
+	while ( myPtr != NULL ) {
+		switch ( counter ) {
+			case 12: // utilization
+				memcpy ( (void*) buff.nvidiaLines[1], myPtr, strlen ( myPtr ) - 1 );
+				break;
+			case 2: // get temp
+				memcpy ( (void*) buff.nvidiaLines[0], myPtr, strlen ( myPtr ) - 1 );
+				break;
+			case 4: // get watt
+				memcpy ( (void*) buff.nvidiaLines[2], myPtr, strlen ( myPtr ) - 1 );
+				break;
+		}
+		// next field
+  	myPtr = strtok(NULL, " ");
+		counter++;
+	}
+
+	free ( commandRet );
+}
+
+void getLiquidctlValues ( ) {
+	char* commandRet;
+	char* myPtr;
+	int counter;
+
+	getLinesFromCommand (
+		&commandRet,
+		"/usr/bin/liquidctl status | tail -n6 | head -n5");
+
+	// divide string
+	myPtr = strtok( commandRet, " ");
+	counter = 0;
+
+	while ( myPtr != NULL ) {
+		switch ( counter ) {
+			case 11: // utilization
+				memcpy ( (void*) buff.liquidLines[1], myPtr, strlen ( myPtr ) + 1 );
+				break;
+			case 3: // get temp
+				memcpy ( (void*) buff.liquidLines[2], myPtr, strlen ( myPtr ) + 1 );
+				break;
+			case 19: // get temp
+				memcpy ( (void*) buff.liquidLines[0], myPtr, strlen ( myPtr ) + 1 );
+				break;
+		}
+		// next field
+  	myPtr = strtok(NULL, " ");
+		counter++;
+	}
+}
+
+void getSystemValues ( ) {
+	char* commandRet;
+	char* myPtr;
+	int counter;
+
+	getLinesFromCommand (
+		&commandRet,
+		"sensors | grep \"System Fan\"");
+
+	// divide string
+	myPtr = strtok( commandRet, " ");
+	counter = 0;
+
+	while ( myPtr != NULL ) {
+		switch ( counter ) {
+			case 15: // utilization
+				memcpy ( (void*) buff.systemLines[0], myPtr, strlen ( myPtr ) + 1 );
+				break;
+			case 27: // utilization
+				memcpy ( (void*) buff.systemLines[1], myPtr, strlen ( myPtr ) + 1 );
+				break;
+			case 39: // utilization
+				memcpy ( (void*) buff.systemLines[2], myPtr, strlen ( myPtr ) + 1 );
+				break;
+		}
+		// next field
+		myPtr = strtok(NULL, " ");
+		counter++;
+	}
+}
+
+void getValues ( ) {
+	getCpuValues ( );
+	getGpuValues ( );
+	getLiquidctlValues ( );
+	getSystemValues ( );
 }
 
 
@@ -172,7 +339,7 @@ void writeSerial ( ) {
 
 #ifdef DEBUG
 	 if ( isDebug )
-	 	 printf ( buffer );
+	 	 printf ( "%s\n", buffer );
 #endif
 
 	 write ( serialPort, buffer, sizeof ( buffer ) );
@@ -227,31 +394,22 @@ inline void debFillBufferTestData ( ) {
  * DEBUG outputs buffer struct
  */
 void debOutputBuffer ( ) {
-	register unsigned int counter = 0;
-
-	printf ( "cpu related\n" );
-	for (counter = 0; counter < 3; counter++ ) {
-		printf( ( const char* ) buff.cpuLines[counter] );
-		printf("\n");
-	}
-
-	printf ( "watercool related\n" );
-	for (counter = 0; counter < 3; counter++ ) {
-		printf( ( const char* ) buff.liquidLines[counter] );
-		printf("\n");
-	}
-
-	printf ( "gpu related\n" );
-	for (counter = 0; counter < 3; counter++ ) {
-		printf( ( const char* ) buff.nvidiaLines[counter] );
-		printf("\n");
-	}
-
-	printf ( "system related\n" );
-	for (counter = 0; counter < 3; counter++ ) {
-		printf( ( const char* ) buff.systemLines[counter] );
-		printf("\n");
-	}
+	printf("GPU Temp: %s\n    utilization: %s\n    Watt: %s\n",
+		buff.nvidiaLines[0],
+		buff.nvidiaLines[1],
+		buff.nvidiaLines[2]);
+	printf("CPU Temp: %s\n    takt: %s\n    utilization: %s\n",
+	  buff.cpuLines[0],
+		buff.cpuLines[1],
+		buff.cpuLines[2]);
+  printf("Liq fanspeed: %s\n    pumpspeed: %s\n    watertemp: %s\n",
+		buff.liquidLines[0],
+	  buff.liquidLines[1],
+		buff.liquidLines[2]);
+	printf("SYS fan1: %s\n    fan2: %s\n    fan3: %s\n",
+		buff.systemLines[0],
+		buff.systemLines[1],
+		buff.systemLines[2]);
 }
 #endif
 
@@ -276,7 +434,7 @@ void init_monitor ( ) {
 	openSerial ( );
 
 #ifdef DEBUG
-	debFillBufferTestData ( );
+	//debFillBufferTestData ( );
 #endif
 }
 
@@ -305,21 +463,30 @@ void close_monitor ( ) {
 
 
 
-/*
+/**
  * Like arduino loop() function. whitch contains the main loop of
  * recieving data from lm_sensors, parse data and sending it over serial.
- * @TODO
- *
- * - generate while(1) loop
- * - signal handling for SIGKILL
  */
 void loop_monitor ( ) {
+	while ( 1 ) {
+		getValues ( );
 #ifdef DEBUG
 	if ( isDebug )
 		debOutputBuffer ( );
 #endif
+		writeSerial ( );
+		sleep ( 1 );
+	}
+}
 
-	writeSerial ( );
+
+
+void sig_handler(int signo)
+{
+  if (signo == SIGINT) {
+    printf("received SIGINT, closing watchdog\n");
+		exit ( EXIT_SUCCESS );
+	}
 }
 
 
@@ -338,8 +505,11 @@ int main (int argc, char** argv) {
 	serialPort = 0;
 	optionIndex = 0;
 	isDevicePathChanged = 0;
+
 	// exit handler for cleanup
-	atexit(close_monitor);
+	atexit ( close_monitor );
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
+	printf("\ncan't catch SIGINT\n");
 
 	// opt management
 	while (1) {
@@ -347,20 +517,16 @@ int main (int argc, char** argv) {
 
 		if ( opt == -1 ) break;
 
-		swhitch ( opt ) {
-
+		switch ( opt ) {
 			case 'h':
 				showHelp ( );
 				exit ( EXIT_SUCCESS );
-
 			case 'v':
 				showVersion ( );
 				exit ( EXIT_SUCCESS );
-
 			case 'D':
 				isDebug = 1;
 				break;
-
 			case 'p':
 				devicePath = malloc ( sizeof ( optarg ) * strlen ( optarg ) );
 				memcpy ( devicePath, optarg, strlen ( optarg ) );
