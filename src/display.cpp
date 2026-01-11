ffc3a6bae18d0f3f5a6c4692fe93ddd643798ba2
N. Neumann <vatriani.nn@googlemail.com>
2026-01-11 19:59:21 +0100

    Pointer errror bug

--- src/display.cpp
+++ src/display.cpp
@@ -71,38 +71,38 @@ void Display::drawContend ( ) {
   switch ( actualPage ) {
   case CPU:
     lineText[0].append ( displayString.t );
-    sReplace ( &lineText[0], data.cpu_t );
+    sReplace ( &lineText[0], data->cpu_t );
     lineText[1].append ( displayString.c );
-    sReplace ( &lineText[1], data.cpu_c );
+    sReplace ( &lineText[1], data->cpu_c );
     lineText[2].append ( displayString.u );
-    sReplace ( &lineText[2], data.cpu_u );
+    sReplace ( &lineText[2], data->cpu_u );
     break;;
 
   case LIQUID:
     lineText[0].append ( displayString.s );
-    sReplace ( &lineText[0], data.liquid_f );
+    sReplace ( &lineText[0], data->liquid_f );
     lineText[1].append ( displayString.s );
-    sReplace ( &lineText[1], data.liquid_p );
+    sReplace ( &lineText[1], data->liquid_p );
     lineText[2].append ( displayString.t );
-    sReplace ( &lineText[2], data.liquid_t );
+    sReplace ( &lineText[2], data->liquid_t );
     break;;
 
   case GPU:
     lineText[0].append ( displayString.t );
-    sReplace ( &lineText[0], data.gpu_t );
+    sReplace ( &lineText[0], data->gpu_t );
     lineText[1].append ( displayString.u );
-    sReplace ( &lineText[1], data.gpu_u );
+    sReplace ( &lineText[1], data->gpu_u );
     lineText[2].append ( displayString.p );
-    sReplace ( &lineText[2], data.gpu_p );
+    sReplace ( &lineText[2], data->gpu_p );
     break;;
 
   case SYS:
     lineText[0].append ( displayString.s );
-    sReplace ( &lineText[0], data.fan1_s );
+    sReplace ( &lineText[0], data->fan1_s );
     lineText[1].append ( displayString.s );
-    sReplace ( &lineText[1], data.fan2_s );
+    sReplace ( &lineText[1], data->fan2_s );
     lineText[2].append ( displayString.s );
-    sReplace ( &lineText[2], data.fan3_s );
+    sReplace ( &lineText[2], data->fan3_s );
     break;;
   }
 
@@ -150,7 +150,7 @@ void Display::next ( ) {
 
 
 void Display::setDisplayData ( displayData *dataN ) {
-  this->data = *dataN;
+  this->data = dataN;
 }
 
 
